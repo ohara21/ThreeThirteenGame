732a3edfc7ae3a9759ac6f14f9d6765b6b9de2b1
Nick <ohara21@up.edu>
2020-11-09 12:02:59 -0800

    cleaned up GameBoard

--- app/src/main/java/edu/up/threethirteengame/TTGame/GameBoard.java
+++ app/src/main/java/edu/up/threethirteengame/TTGame/GameBoard.java
@@ -31,8 +31,8 @@ public class GameBoard extends SurfaceView {
     TTGameState ttGameState = null;
 
     //instance variables that will define locations on the GameBoard
-    float viewWidth;
-    float viewHeight;
+    float sectionWidth;
+    float sectionHeight;
     float padx = 35;
     float pady = 4;
 
@@ -45,65 +45,6 @@ public class GameBoard extends SurfaceView {
 
     //used to rotate the card images
     Matrix rotate = new Matrix();
-
-    //creating a deck of 52 cards
-    char suite[] = new char[] {'c','s','h','d'};
-    ArrayList<Card> deck = new ArrayList<Card>();
-
-    //create all 53 card objects
-    Card backCard = new Card(2);
-    Card aceClubsCard = new Card(1,'c',1);
-    Card aceHeartsCard = new Card(1,'h',1);
-    Card aceSpadesCard = new Card(1,'s',1);
-    Card aceDiamondsCard = new Card(1,'d',1);
-    Card twoClubsCard = new Card(1,'c',2);
-    Card twoHeartsCard = new Card(1,'h',2);
-    Card twoSpadesCard = new Card(1,'s',2);
-    Card twoDiamondsCard = new Card(1,'d',2);
-    Card threeClubsCard = new Card(1,'c',3);
-    Card threeHeartsCard = new Card(1,'h',3);
-    Card threeSpadesCard = new Card(1,'s',3);
-    Card threeDiamondsCard = new Card(1,'d',3);
-    Card fourClubsCard = new Card(1,'c',4);
-    Card fourHeartsCard = new Card(1,'h',4);
-    Card fourSpadesCard = new Card(1,'s',4);
-    Card fourDiamondsCard = new Card(1,'d',4);
-    Card fiveClubsCard = new Card(1,'c',5);
-    Card fiveHeartsCard = new Card(1,'h',5);
-    Card fiveSpadesCard = new Card(1,'s',5);
-    Card fiveDiamondsCard = new Card(1,'d',5);
-    Card sixClubsCard = new Card(1,'c',6);
-    Card sixHeartsCard = new Card(1,'h',6);
-    Card sixSpadesCard = new Card(1,'s',6);
-    Card sixDiamondsCard = new Card(1,'d',6);
-    Card sevenClubsCard = new Card(1,'c',7);
-    Card sevenHeartsCard = new Card(1,'h',7);
-    Card sevenSpadesCard = new Card(1,'s',7);
-    Card sevenDiamondsCard = new Card(1,'d',7);
-    Card eightClubsCard = new Card(1,'c',8);
-    Card eightHeartsCard = new Card(1,'h',8);
-    Card eightSpadesCard = new Card(1,'s',8);
-    Card eightDiamondsCard = new Card(1,'d',8);
-    Card nineClubsCard = new Card(1,'c',9);
-    Card nineHeartsCard = new Card(1,'h',9);
-    Card nineSpadesCard = new Card(1,'s',9);
-    Card nineDiamondsCard = new Card(1,'d',9);
-    Card tenClubsCard = new Card(1,'c',10);
-    Card tenHeartsCard = new Card(1,'h',10);
-    Card tenSpadesCard = new Card(1,'s',10);
-    Card tenDiamondsCard = new Card(1,'d',10);
-    Card jackClubsCard = new Card(1,'c',11);
-    Card jackHeartsCard = new Card(1,'h',11);
-    Card jackSpadesCard = new Card(1,'s',11);
-    Card jackDiamondsCard = new Card(1,'d',11);
-    Card queenClubsCard = new Card(1,'c',12);
-    Card queenHeartsCard = new Card(1,'h',12);
-    Card queenSpadesCard = new Card(1,'s',12);
-    Card queenDiamondsCard = new Card(1,'d',12);
-    Card kingClubsCard = new Card(1,'c',13);
-    Card kingHeartsCard = new Card(1,'h',13);
-    Card kingSpadesCard = new Card(1,'s',13);
-    Card kingDiamondsCard = new Card(1,'d',13);
 
     /**
      * constructors that were inherited from SurfaceView class
@@ -130,19 +71,25 @@ public class GameBoard extends SurfaceView {
 
     /**
      * This function draws the specified card object to a given x and y on a given canvas
-     * Works for both front and back showing cards
-     */
-    /** External Citation:
-     * Date: 9/18/20
-     * Problem: Didn't know how to create Bitmap image
-     * Resource: Professor Nuxoll
-     * Solution: used his code
+     * works for both front and back showing cards
+     * @param canvas the canvas that's to be drawn on
+     * @param x the x location of the card
+     * @param y the y location of the card
+     * @param card the card to be drawn
      */
     public void drawCard(Canvas canvas, float x, float y, Card card){
+
+        /** External Citation:
+         * Date: 9/18/20
+         * Problem: Didn't know how to create Bitmap image
+         * Resource: Professor Nuxoll
+         * Solution: used his code
+         */
 
         Bitmap tempObj = BitmapFactory.decodeResource(getResources(), card.getCardId());
         Bitmap cardObj = Bitmap.createScaledBitmap(tempObj,card.getWidth(),card.getHeight(),true);
 
+        //draws the card first, then the border
         canvas.drawBitmap(cardObj, x, y, null);
         drawCardBorder(canvas,x,y,card);
     }
@@ -227,10 +174,10 @@ public class GameBoard extends SurfaceView {
 
     /**
      * draws a rotated card, used for the discard and stock pile
-     * @param canvas
-     * @param x
-     * @param y
-     * @param card
+     * @param canvas the canvas that's to be drawn on
+     * @param x the x location of the card
+     * @param y the y location of the card
+     * @param card the card to be drawn
      */
     /**
      * External Citation:
@@ -264,8 +211,8 @@ public class GameBoard extends SurfaceView {
      */
     @Override
     public void onDraw(Canvas canvas){
-        float sectionWidth = this.getWidth()/4;
-        float sectionHeight = this.getHeight()/5;
+        sectionWidth = this.getWidth()/4;
+        sectionHeight = this.getHeight()/5;
         if(ttGameState == null){
             return;
         }
