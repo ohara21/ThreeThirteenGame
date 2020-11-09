dfa214f3ddd14db384630707f60cf290f7e73c74
adrianmuth <muth23@up.edu>
2020-11-08 19:06:23 -0800

    Functioning quit button.

--- app/src/main/java/edu/up/threethirteengame/TTGame/TTHumanPlayer.java
+++ app/src/main/java/edu/up/threethirteengame/TTGame/TTHumanPlayer.java
@@ -19,7 +19,7 @@ import edu.up.threethirteengame.game.GameFramework.infoMessage.GameInfo;
 import edu.up.threethirteengame.game.GameFramework.infoMessage.IllegalMoveInfo;
 import edu.up.threethirteengame.game.GameFramework.infoMessage.NotYourTurnInfo;
 
-public class TTHumanPlayer extends GameHumanPlayer {
+public class TTHumanPlayer extends GameHumanPlayer implements View.OnClickListener {
 
     //our game state
     private TTGameState state;
@@ -33,7 +33,6 @@ public class TTHumanPlayer extends GameHumanPlayer {
     private Button restartButton;
     private Button goOutButton;
     private Button discardButton;
-    private Button selectCardsButton;
     private Button removeGroupButton;
     private Button addGroupButton;
 
@@ -109,11 +108,45 @@ public class TTHumanPlayer extends GameHumanPlayer {
      */
     @Override
     public void setAsGui(GameMainActivity activity) {
+       // Getting xml elements
         myActivity.setContentView(R.layout.tt_human_player);
         roundText = (TextView) myActivity.findViewById(R.id.roundText);
         yourScoreText = (TextView) myActivity.findViewById(R.id.yourScoreText);
         opponScoreText = (TextView) myActivity.findViewById(R.id.opponScoreText);
         gameBoard = myActivity.findViewById(R.id.surfaceView);
+
+        // Setting onClick listeners for buttons
+        helpButton.setOnClickListener(this);
+        quitButton.setOnClickListener(this);
+        restartButton.setOnClickListener(this);
+        goOutButton.setOnClickListener(this);
+        discardButton.setOnClickListener(this);
+        addGroupButton.setOnClickListener(this);
+        removeGroupButton.setOnClickListener(this);
     }
 
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case (R.id.helpButton):
+                break;
+            case (R.id.quitButton):
+                // Close program
+                System.exit(0);
+                break;
+            case (R.id.restartButton):
+                break;
+            case (R.id.goOutButton):
+                break;
+            case (R.id.discardButton):
+                break;
+            case (R.id.addGroup):
+                break;
+            case (R.id.removeGroup):
+                break;
+            default:
+                // do nothing
+                break;
+        }
+    }
 }
